faf913efbaa5a07f21e50a159b34f25ffce8b477
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-08-04 22:29:36 +0300

    removed redundant

--- src/components/Game/GameField.tsx
+++ src/components/Game/GameField.tsx
@@ -14,10 +14,6 @@ type Coordinates = {
 const GameField = ({ width, height }: GameFieldProps) => {
   const count = width * height;
   const [cells, setCells] = useState<Array<boolean>>(Array(count).fill(false));
-  const ww = `repeat(${width},_1fr)`;
-  const hh = `repeat(${height},_1fr)`;
-  const gridCols = `grid-cols-[${ww}]`;
-  const gridRows = `grid-rows-[${hh}]`;
 
   return (
     <div className="flex flex-col">
@@ -29,11 +25,7 @@ const GameField = ({ width, height }: GameFieldProps) => {
       >
         click me
       </button>
-      <div
-        // className={`grid gap-2 grid-cols-[_repeat(${width},_1fr)] grid-rows-[_repeat(${height},_1fr)]`}
-        // className={`grid gap-2 ${gridCols} ${gridRows}`}
-        className={`grid gap-2 grid-cols-${width} grid-rows-${height}`}
-      >
+      <div className={`grid gap-2 grid-cols-${width} grid-rows-${height}`}>
         {Array.from({ length: count }, (_, i) => (
           <Cell key={i} index={i} />
         ))}
