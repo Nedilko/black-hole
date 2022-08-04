183a0f4b416dac72449df279a4cd630b4083f0b8
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-08-04 22:25:20 +0300

    fixed grid

--- src/components/Game/GameField.tsx
+++ src/components/Game/GameField.tsx
@@ -14,6 +14,10 @@ type Coordinates = {
 const GameField = ({ width, height }: GameFieldProps) => {
   const count = width * height;
   const [cells, setCells] = useState<Array<boolean>>(Array(count).fill(false));
+  const ww = `repeat(${width},_1fr)`;
+  const hh = `repeat(${height},_1fr)`;
+  const gridCols = `grid-cols-[${ww}]`;
+  const gridRows = `grid-rows-[${hh}]`;
 
   return (
     <div className="flex flex-col">
@@ -26,7 +30,9 @@ const GameField = ({ width, height }: GameFieldProps) => {
         click me
       </button>
       <div
-        className={`grid gap-2 grid-cols-[_repeat(${width},_1fr)] grid-rows-[_repeat(${height},_1fr)]`}
+        // className={`grid gap-2 grid-cols-[_repeat(${width},_1fr)] grid-rows-[_repeat(${height},_1fr)]`}
+        // className={`grid gap-2 ${gridCols} ${gridRows}`}
+        className={`grid gap-2 grid-cols-${width} grid-rows-${height}`}
       >
         {Array.from({ length: count }, (_, i) => (
           <Cell key={i} index={i} />
