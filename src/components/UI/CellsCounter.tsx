ad7fe8eb3cdc8657dbb62aa84ed511cb6c109b59
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-08-18 15:50:46 +0300

    UI adjucements

--- src/components/UI/CellsCounter.tsx
+++ src/components/UI/CellsCounter.tsx
@@ -11,7 +11,6 @@ const CellsCounter = () => {
     <>
       <span className="flex w-5 h-5 border-2 border-gray-400/80 mr-2 rounded-full" />
       {`${remainingCellsCount} / ${totalCellsCount}`}
-      <span className="flex w-5 h-5 border-2 border-gray-400/80 ml-2" />
     </>
   );
 };
--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -9,9 +9,9 @@ const Header = () => {
     (state: RootState) => state.game
   );
   return (
-    <div className="flex justify-center w-full items-center text-5xl text-gray-300/80 uppercase text-shadow">
-      <div className="flex">find black holes</div>
-      <div className="flex justify-between absolute w-full px-4">
+    <div className="flex flex-col justify-center w-full items-center text-5xl text-gray-300/80 uppercase text-shadow">
+      <div className="flex text-center">find black holes</div>
+      <div className="flex justify-between sm:absolute w-full px-4 mt-2 sm:mt-0">
         <div className="py-21 flex text-lg items-center">
           {showCounter && <CellsCounter />}
         </div>
