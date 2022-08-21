926471e9c176a9f9ed5d6ce0bbd236119934dfd5
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-08-21 21:53:29 +0300

    header adjustements

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,9 @@ function App() {
   return (
     <div className="font-extralight bg-gradient-to-b from-[#06070d] via-[#061234] to-[#0e103e]">
       <div className="flex flex-col min-h-screen p-4 overflow-x-scroll">
-        <header>{!showSettings && <Header />}</header>
+        <header>
+          <Header />
+        </header>
         <main className="flex flex-col mt-24 justify-center items-center">
           {showSettings && <SettingsDialog />}
           {isWon && <Results />}
