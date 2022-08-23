c129b1e0748de7bd03ae9399ebf585f8ac6ba793
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-08-23 21:11:57 +0300

    refactoring

--- src/components/UI/__tests__/Timer.test.tsx
+++ src/components/UI/__tests__/Timer.test.tsx
@@ -4,17 +4,19 @@ import { renderWithProviders } from '../../../utils/test-utils';
 import { screen } from '@testing-library/react';
 import Timer from '../Timer';
 
-it('should render Timer', () => {
-  const { container } = renderWithProviders(<Timer />);
-  expect(container).toMatchSnapshot();
-});
+describe('Timer', () => {
+  it('should render Timer', () => {
+    const { container } = renderWithProviders(<Timer />);
+    expect(container).toMatchSnapshot();
+  });
 
-it('should change timer text', () => {
-  const store = setupStore();
-  store.dispatch(updateTime());
-  store.dispatch(updateTime());
+  it('should change timer text', () => {
+    const store = setupStore();
+    store.dispatch(updateTime());
+    store.dispatch(updateTime());
 
-  renderWithProviders(<Timer />, { store });
+    renderWithProviders(<Timer />, { store });
 
-  expect(screen.getByText(/00:02/)).toBeInTheDocument();
+    expect(screen.getByText(/00:02/)).toBeInTheDocument();
+  });
 });
