326a23da3697b7d7f26e034a497a91a835640e76
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-08-25 02:26:56 +0300

    enhanced Timer tests

--- src/components/UI/__tests__/Timer.test.tsx
+++ src/components/UI/__tests__/Timer.test.tsx
@@ -1,10 +1,13 @@
 import { setupStore } from '../../../store';
-import { updateTime } from '../../../store/gameSlice';
+import * as actions from '../../../store/gameSlice';
 import { renderWithProviders } from '../../../utils/test-utils';
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import Timer from '../Timer';
 
 describe('Timer', () => {
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
   it('should render Timer', () => {
     const { container } = renderWithProviders(<Timer />);
     expect(container).toMatchSnapshot();
@@ -12,11 +15,34 @@ describe('Timer', () => {
 
   it('should change timer text', () => {
     const store = setupStore();
-    store.dispatch(updateTime());
-    store.dispatch(updateTime());
+    store.dispatch(actions.updateTime());
+    store.dispatch(actions.updateTime());
 
     renderWithProviders(<Timer />, { store });
 
     expect(screen.getByText(/00:02/)).toBeInTheDocument();
+  });
+
+  it('should update time', () => {
+    jest.useFakeTimers();
+
+    const store = setupStore();
+    store.dispatch(actions.startCountdown());
+
+    renderWithProviders(<Timer />, { store });
+
+    const updateTimeAction = jest.spyOn(actions, 'updateTime');
+
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+
+    expect(updateTimeAction).toHaveBeenCalledTimes(2);
+
+    jest.useRealTimers();
+    updateTimeAction.mockRestore();
   });
 });
